5b01dda221da92c78bbe4f6074cc9d8800e39342
Jack Conrad Clark <clark.jack0@gmail.com>
2019-09-07 14:56:18 -0500

    added routing

--- HH/mainApp/views.py
+++ HH/mainApp/views.py
@@ -6,3 +6,7 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+class AboutPageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'about.html', context=None)
