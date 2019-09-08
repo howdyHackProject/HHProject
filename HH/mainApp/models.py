fd1ec902648636395fae7d843fbb68550ec5f6c4
insrt <sumodude529@gmail.com>
2019-09-07 20:09:05 -0500

    added user model

--- HH/mainApp/models.py
+++ HH/mainApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.User):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    interests = models.CharField(max_length=500)
