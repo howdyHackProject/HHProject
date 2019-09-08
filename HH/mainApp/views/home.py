fddd85fe6d8392fee4fe6efc6db847b9d81099b7
Jack Conrad Clark <clark.jack0@gmail.com>
2019-09-08 09:56:07 -0500

    adding shit

--- HH/mainApp/views/home.py
+++ HH/mainApp/views/home.py
@@ -9,12 +9,19 @@ class HomePage(TemplateView):
 
     # run when user first signs up
     def get(self, request, **kwargs):
-        if (request.method == "POST"):
-            user = User()
-            user.firstName = request.POST.get("first_name")
-            user.lastName = request.POST.get("last_name")
-            user.age = request.POST.get("age")
-            user.username = request.POST.get("username")
-            user.password = request.POST.get("password")
-            user.save()
-        return render(request, 'homePage.html', context=None)
+        # context = {'temp' : "this function is being run"}
+        # if (request.method != "POST"):
+        #     return render(request, 'homePage.html', context=None)
+        # print("Request method was POST")
+        user = User()
+        user.firstName = request.GET.get("first_name")
+        print(user.firstName)
+        user.lastName = request.GET.get("last_name")
+        user.age = request.GET.get("age")
+        user.username = request.GET.get("username")
+        user.password = request.GET.get("password")
+        user.save()
+
+        context = {'u' : user}
+
+        return render(request, 'homePage.html', context)
