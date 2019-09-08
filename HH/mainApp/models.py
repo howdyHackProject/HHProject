a34cdf63ac68154086d5ea66edb67f7b0c3af095
Jack Conrad Clark <clark.jack0@gmail.com>
2019-09-07 20:09:54 -0500

    syntax fix

--- HH/mainApp/models.py
+++ HH/mainApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class User(models.User):
+class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     interests = models.CharField(max_length=500)
